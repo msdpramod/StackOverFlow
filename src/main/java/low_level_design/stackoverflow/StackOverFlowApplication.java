971876a87ec31fa1ca433af9ffdfd36a769a5121
kuppapramod <kuppapramod@gmail.com>
2024-09-17 20:22:33 +0530

    welcome to StackOverFlow

---
+++ src/main/java/low_level_design/stackoverflow/StackOverFlowApplication.java
@@ -0,0 +1,13 @@
+package low_level_design.stackoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackOverFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StackOverFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/low_level_design/stackoverflow/StackOverFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package low_level_design.stackoverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackOverFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
